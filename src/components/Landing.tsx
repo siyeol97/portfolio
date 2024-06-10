8cfa73f1bb16ec3f01d12f14d806b228f39c8e11
siyeol97 <leesi2830@naver.com>
2024-06-10 13:00:55 +0900

    💄 Style: glitch font 변경, 함수분리

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,30 +1,15 @@
 import LandingBackground from './LandingBackground';
 import styles from './Landing.module.scss';
 import { useEffect } from 'react';
+import glitch from '../utils/glitch';
 
 export default function Landing() {
   useEffect(() => {
-    const text = document.querySelector('h1');
+    const text = document.getElementById('glitch');
 
     if (!text) {
       return;
     }
-
-    const glitch = (element: HTMLHeadingElement) => {
-      setInterval(() => {
-        const degree = Math.random() * 20 - 10;
-        const beforeTop = Math.random() * 100;
-        const beforeBottom = Math.random() * 100;
-        const afterTop = Math.random() * 100;
-        const afterBottom = Math.random() * 100;
-
-        element.style.setProperty('--degree', `${degree}deg`);
-        element.style.setProperty('--bt', `${beforeTop}%`);
-        element.style.setProperty('--at', `${afterTop}%`);
-        element.style.setProperty('--bb', `${beforeBottom}%`);
-        element.style.setProperty('--ab', `${afterBottom}%`);
-      }, 100);
-    };
 
     glitch(text);
   }, []);
@@ -32,7 +17,12 @@ export default function Landing() {
   return (
     <section className={styles.wrapper}>
       <div className={styles.greeting}>
-        <h1 data-text='Hello world!'>Hello world!</h1>
+        <h1
+          data-text='Hello world!'
+          id='glitch'
+        >
+          Hello world!
+        </h1>
       </div>
       <LandingBackground />
     </section>
---
+++ src/utils/glitch.ts
@@ -0,0 +1,30 @@
+const glitch = (element: HTMLElement) => {
+  let count = 0;
+  setInterval(() => {
+    const degree = Math.random() * 20 - 10;
+    const beforeTop = Math.random() * 100;
+    const beforeBottom = Math.random() * 100;
+    const afterTop = Math.random() * 100;
+    const afterBottom = Math.random() * 100;
+
+    element.style.setProperty('--degree', `${degree}deg`);
+    element.style.setProperty('--bt', `${beforeTop}%`);
+    element.style.setProperty('--at', `${afterTop}%`);
+    element.style.setProperty('--bb', `${beforeBottom}%`);
+    element.style.setProperty('--ab', `${afterBottom}%`);
+    element.style.setProperty('--scale', '1');
+
+    count++;
+
+    if (count % 15 === 0) {
+      const bigDegree = Math.random() * 180 - 90;
+      element.style.setProperty('--degree', `${bigDegree}deg`);
+    }
+    if (count % 30 === 0) {
+      const bigScale = 1 + Math.random() / 2;
+      element.style.setProperty('--scale', `${bigScale}`);
+    }
+  }, 100);
+};
+
+export default glitch;
