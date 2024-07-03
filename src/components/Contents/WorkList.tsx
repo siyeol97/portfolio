3d620300496827e0a386b8203df60e15d3a5ae9b
siyeol97 <leesi2830@naver.com>
2024-07-03 19:09:44 +0900

    💄 Style: add work list image

--- src/components/Contents/WorkList.tsx
+++ src/components/Contents/WorkList.tsx
@@ -2,12 +2,32 @@ import { useState } from 'react';
 import Detail from './Detail';
 import styles from './WorkList.module.scss';
 import arrowBack from '../../public/arrow-back.svg';
+import blog_cover_image from '../../public/blog_cover.png';
+import diary_cover_image from '../../public/diary_cover.png';
+import mogazoa_cover_image from '../../public/mogazoa_cover.png';
+import ptj_cover_image from '../../public/ptj_cover.png';
 
 const cards = [
-  { title: 'Project 1', description: 'Description 1' },
-  { title: 'Project 2', description: 'Description 2' },
-  { title: 'Project 3', description: 'Description 3' },
-  { title: 'Project 4', description: 'Description 4' },
+  {
+    title: 'mogazoa',
+    description: 'Description 1',
+    image: mogazoa_cover_image,
+  },
+  {
+    title: 'party-time-job',
+    description: 'Description 2',
+    image: ptj_cover_image,
+  },
+  {
+    title: `Siyeol's Dev-log`,
+    description: 'Description 3',
+    image: blog_cover_image,
+  },
+  {
+    title: '등잔 밑 일기',
+    description: 'Description 4',
+    image: diary_cover_image,
+  },
 ];
 
 export default function WorkList() {
@@ -37,6 +57,12 @@ export default function WorkList() {
             className={`${styles.card} ${isActive === index ? styles.active : isActive !== null ? styles.hidden : ''}`}
             onClick={() => handleClick(index)}
           >
+            <div className={styles.background_image}>
+              <img
+                src={card.image}
+                alt='background_image'
+              />
+            </div>
             <img
               src={arrowBack}
               alt='arrow-back'
