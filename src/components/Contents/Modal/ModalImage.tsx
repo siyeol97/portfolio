24c68452fd9ff1c726059c10aa4758bfd6f00e97
siyeol97 <leesi2830@naver.com>
2024-11-01 18:30:06 +0900

    💄 Fix: carousel arrow

--- src/components/Contents/Modal/ModalImage.tsx
+++ src/components/Contents/Modal/ModalImage.tsx
@@ -2,6 +2,8 @@ import styles from './ModalImage.module.scss';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import arrowNext from '../../../public/asset/arrow-next.svg';
+import arrowPrev from '../../../public/asset/arrow-prev.svg';
 
 interface Props {
   images: string[];
@@ -14,6 +16,8 @@ export default function ModalImage({ images }: Props) {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
   return (
     <section className={styles.wrapper}>
@@ -34,3 +38,39 @@ export default function ModalImage({ images }: Props) {
     </section>
   );
 }
+
+interface ArrowProps {
+  className?: string;
+  style?: React.CSSProperties;
+  onClick?: () => void;
+}
+
+function NextArrow({ className, style, onClick }: ArrowProps) {
+  return (
+    <img
+      src={arrowNext}
+      alt='arrow-next'
+      onClick={onClick}
+      style={{ width: '40px', height: '40px', zIndex: 1, ...style }}
+      className={`${styles.arrow} ${className}`}
+    />
+  );
+}
+
+function PrevArrow({ className, style, onClick }: ArrowProps) {
+  return (
+    <img
+      src={arrowPrev}
+      alt='arrow-prev'
+      onClick={onClick}
+      style={{ ...arrowStyle, ...style }}
+      className={`${styles.arrow} ${className}`}
+    />
+  );
+}
+
+const arrowStyle = {
+  width: '40px',
+  height: '40px',
+  zIndex: 1,
+};
