62b14d4067d3492ff20dc51e03650f0d02aeccd9
siyeol97 <leesi2830@naver.com>
2024-11-08 10:17:26 +0900

    🚚 Fix: delete Landing

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from './App';
 import Contents from './components/Contents/Contents';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
-  },
-  {
-    path: '/contents',
     element: <Contents />,
   },
 ]);
